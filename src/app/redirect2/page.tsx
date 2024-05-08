72edf8ea11ebd6e29edd9d3c0d1311904aebe956
Abdallah Abdel-khalek <abdallah.abdelkhalek98@gmail.com>
2024-05-08 11:52:49 +0300

    url update

--- src/app/redirect2/page.tsx
+++ src/app/redirect2/page.tsx
@@ -2,7 +2,7 @@
 import Redirect from '@/components/Redirect';
 
 const page = () => {
-  return <Redirect url="exp://127.0.0.1:8081?param1=123&param2=value2" />;
+  return <Redirect url="exp://192.168.1.26:8081?param1=123&param2=value2" />;
 };
 
 export default page;
